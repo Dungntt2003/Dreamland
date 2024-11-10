5582467acf13ba0d394575165be3a67fa00c8706
Dungntt2003 <nguyenthuydung55555@gmail.com>
2024-11-10 15:54:36 +0700

    change local to session to store token

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,5 +1,6 @@
-import React, { createContext, useState, useContext } from "react";
+import React, { createContext, useState, useContext, useEffect } from "react";
 import { jwtDecode } from "jwt-decode";
+import { v4 as uuidv4 } from "uuid";
 
 const AuthContext = createContext();
 
@@ -7,17 +8,32 @@ export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [role, setRole] = useState(null);
   const login = (token) => {
-    localStorage.setItem("token", token);
+    const sessionId = uuidv4();
+    sessionStorage.setItem("sessionId", sessionId);
+    sessionStorage.setItem(`token_${sessionId}`, token);
     setIsAuthenticated(true);
     const decodedToken = jwtDecode(token);
     setRole(decodedToken.role);
   };
 
   const logout = () => {
-    localStorage.removeItem("token");
+    const sessionId = sessionStorage.getItem("sessionId");
+    if (sessionId) {
+      sessionStorage.removeItem(`token_${sessionId}`);
+      sessionStorage.removeItem("sessionId");
+    }
     setIsAuthenticated(false);
     setRole(null);
   };
+
+  useEffect(() => {
+    const sessionId = sessionStorage.getItem("sessionId");
+    const token = sessionStorage.getItem(`token_${sessionId}`);
+    if (token) {
+      setIsAuthenticated(true);
+      setRole(jwtDecode(token).role);
+    }
+  }, []);
 
   return (
     <AuthContext.Provider value={{ isAuthenticated, role, login, logout }}>
