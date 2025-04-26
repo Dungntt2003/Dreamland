6bfd44cee3f6589597a244768ef67fc25f2af779
Dungntt2003 <nguyenthuydung55555@gmail.com>
2025-04-27 00:28:56 +0700

    Add ui step 2: order info

--- src/views/pages/payment-service/process/enter-process.jsx
+++ src/views/pages/payment-service/process/enter-process.jsx
@@ -1,9 +1,10 @@
 import "./enter-payment.scss";
-import { Steps, Button, Calendar } from "antd";
+import { Steps, Button, Calendar, Form, Input, Checkbox } from "antd";
 import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import repoApi from "api/repoApi";
+import userApi from "api/userApi";
 import entertainmentApi from "api/entertainmentApi";
 import {
   faCartShopping,
@@ -11,9 +12,14 @@ import {
   faCreditCard,
   faMinus,
   faPlus,
+  faSignature,
+  faPhone,
+  faUser,
 } from "@fortawesome/free-solid-svg-icons";
 import formatCurrency from "utils/formatCurrency";
 import dayjs from "dayjs";
+import { useAuth } from "context/authContext";
+import { useForm } from "antd/es/form/Form";
 
 const steps = [
   {
@@ -37,15 +43,39 @@ const parseTimeString = (str) => {
 };
 
 const EnterProcess = () => {
+  const { id } = useAuth();
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
 
   const repoId = queryParams.get("repoId");
   const serviceId = queryParams.get("serviceId");
   const [current, setCurrent] = useState(0);
+  const [form] = useForm();
+  const [user, setUser] = useState({});
   const [enter, setEnter] = useState({});
   const [countAdult, setCountAdult] = useState(0);
   const [countChild, setCountChild] = useState(0);
+  const [isChecked, setIsChecked] = useState(false);
+
+  useEffect(() => {
+    const getInfo = async () => {
+      try {
+        const response = await userApi.getUserInfo(id);
+        setUser(response.data.user);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getInfo();
+  }, [id]);
+
+  useEffect(() => {
+    form.setFieldsValue({
+      name: user.name,
+      email: user.email,
+      phone: user.phone,
+    });
+  }, [user, form]);
 
   useEffect(() => {
     const getRepo = async () => {
@@ -123,7 +153,13 @@ const EnterProcess = () => {
                 }}
               >
                 <div>
-                  <div style={{ textAlign: "center", fontSize: "30px" }}>
+                  <div
+                    style={{
+                      textAlign: "center",
+                      fontSize: "30px",
+                      color: "var(--primary-color)",
+                    }}
+                  >
                     {enter.name}
                   </div>
                   <div style={{ display: "flex", justifyContent: "center" }}>
@@ -140,7 +176,14 @@ const EnterProcess = () => {
                       </div>
                     </div>
                   </div>
-                  <div>
+                  <div
+                    style={{
+                      background: "var(--background-color)",
+                      margin: "16px",
+                      borderRadius: "10px",
+                      padding: "4px",
+                    }}
+                  >
                     <div
                       style={{
                         margin: "16px 0",
@@ -252,9 +295,212 @@ const EnterProcess = () => {
             </>
           )}
           {current === 1 && (
-            <Button type="primary" onClick={next} className="button">
-              Tiếp
-            </Button>
+            <>
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <div
+                  style={{ width: "60%", height: "100%", overflowY: "auto" }}
+                >
+                  <div
+                    style={{ fontSize: "30px", color: "var(--primary-color)" }}
+                  >
+                    Điền thông tin
+                  </div>
+                  <div>
+                    <div className="payment-info-title">Thông tin đơn hàng</div>
+                    <div
+                      style={{
+                        margin: "16px 0",
+                        display: "flex",
+                        padding: "8px",
+                        border: "1px solid #ccc",
+                        borderRadius: "10px",
+                      }}
+                    >
+                      <img
+                        src={enter.images[0]}
+                        alt="anh enter"
+                        style={{
+                          width: "200px",
+                          borderRadius: "10px",
+                          margin: "0 16px",
+                        }}
+                      />
+                      <div>
+                        <div>{enter.name}</div>
+                        <div>Giá: {enter.price}</div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div>
+                    <div className="payment-info-title">Thông tin liên lạc</div>
+                    <p style={{ fontSize: "14px", color: "#333" }}>
+                      Chúng tôi sẽ thông báo mọi thay đổi về đơn hàng cho bạn
+                    </p>
+                    <div>
+                      <Form
+                        form={form}
+                        name="basic"
+                        labelCol={{
+                          span: 4,
+                        }}
+                        wrapperCol={{
+                          span: 16,
+                        }}
+                        style={{
+                          minWidth: 600,
+                          margin: "16px 0",
+                        }}
+                        initialValues={{
+                          name: user.name,
+                          email: user.email,
+                          phone: user.phone,
+                        }}
+                      >
+                        <Form.Item
+                          name="name"
+                          label="Họ và tên"
+                          rules={[
+                            { required: true, message: "Hãy nhập tên mới" },
+                          ]}
+                        >
+                          <Input
+                            prefix={<FontAwesomeIcon icon={faSignature} />}
+                          />
+                        </Form.Item>
+                        <Form.Item
+                          name="email"
+                          label="Email"
+                          rules={[{ required: true }]}
+                        >
+                          <Input
+                            // disabled
+                            prefix={<FontAwesomeIcon icon={faUser} />}
+                          />
+                        </Form.Item>
+                        <Form.Item
+                          name="phone"
+                          label="Số điện thoại"
+                          rules={[
+                            {
+                              required: true,
+                              message: "Hãy nhập số điện thoại mới",
+                            },
+                            {
+                              pattern: /^\d+$/,
+                              message: "Số điện thoại chỉ bao gồm chữ số",
+                            },
+                            {
+                              min: 9,
+                              message: "Số điện thoại yêu cầu ít nhất 9 số",
+                            },
+                            {
+                              max: 11,
+                              message:
+                                "Số điện thoại không được vượt qua 11 số",
+                            },
+                          ]}
+                        >
+                          <Input prefix={<FontAwesomeIcon icon={faPhone} />} />
+                        </Form.Item>
+                      </Form>
+                    </div>
+                  </div>
+                  <div>
+                    <div>
+                      <Checkbox>
+                        Tôi muốn là người đầu tiên nhận các ưu đãi độc quyền qua
+                        email và tin nhắn
+                      </Checkbox>
+                    </div>
+                    <p>
+                      <Checkbox
+                        onChange={(e) => setIsChecked(e.target.checked)}
+                      >
+                        Tôi đã hiểu và đồng ý với Điều khoản sử dụng chung và
+                        Chính sách quyền riêng tư của SmartTrip
+                      </Checkbox>
+                    </p>
+                    <p
+                      style={{
+                        padding: "4px 8px",
+                        background: "#fcf3de",
+                        border: "1px solid red",
+                        borderRadius: "10px",
+                        width: "fit-content",
+                        fontSize: "14px",
+                      }}
+                    >
+                      Vui lòng điền thông tin chính xác. Một khi đã gửi thông
+                      tin, bạn sẽ không thay đổi được.
+                    </p>
+                  </div>
+                  <div
+                    style={{ display: "flex", justifyContent: "space-between" }}
+                  >
+                    <Button type="primary" onClick={prev} className="button">
+                      Trở lại
+                    </Button>
+                    <Button
+                      type="primary"
+                      onClick={next}
+                      className="button"
+                      disabled={!isChecked}
+                    >
+                      Tiếp
+                    </Button>
+                  </div>
+                </div>
+                <div
+                  style={{
+                    width: "40%",
+                    padding: "54px",
+                    position: "sticky",
+                    height: "fit-content",
+                    top: "600",
+                  }}
+                >
+                  <div style={{ fontWeight: "bold", fontSize: "20px" }}>
+                    {enter.name}
+                  </div>
+                  <hr />
+                  <div style={{ color: "#7e7c79", fontSize: "14px" }}>
+                    <div
+                      style={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                      }}
+                    >
+                      <div>Ngày:</div>
+                      <div>{enter.label}</div>
+                    </div>
+                    <div
+                      style={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                      }}
+                    >
+                      <div>Đơn vị:</div>
+                      <div>
+                        Người lớn * {countAdult} <br />
+                        Trẻ em * {countChild}
+                      </div>
+                    </div>
+                  </div>
+                  <hr />
+                  <div
+                    style={{ display: "flex", justifyContent: "space-between" }}
+                  >
+                    <div>Tổng cộng: </div>
+                    <div style={{ color: "red" }}>
+                      {formatCurrency(
+                        (countAdult + countChild) * parsePrice(enter.price)
+                      )}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </>
           )}
           {current === 2 && (
             <Button type="primary" className="button">
